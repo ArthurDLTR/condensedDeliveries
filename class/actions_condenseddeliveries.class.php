25017b108a8d0a834afa51d68bf9fb4d187a79b4
Atu <arthurl52100@gmail.com>
2025-06-06 16:11:56 +0200

    Expédition créée avec produits à l'intérieur, gestion du problème de regénération en cas de rafraichissement

--- class/actions_condenseddeliveries.class.php
+++ class/actions_condenseddeliveries.class.php
@@ -55,7 +55,7 @@ class ActionsCondensedDeliveries {
      * @return  int             -1 to throw an error, 0 if no error
      */
     public function doActions($parameters, $object, $action='create'){
-        global $db, $conf, $langs;
+        global $db, $conf, $langs, $user;
 
         $arrayOrders = GETPOST('toselect', 'array');
         
@@ -72,71 +72,148 @@ class ActionsCondensedDeliveries {
             $expe->socid = $comm->socid;
             $expe->thirdparty = $comm->thirdparty;
             $expe->date = dol_now();
-            $expe->arrayoptions = $comm->arrayoptions;
+            $expe->array_options = $comm->array_options;
 
-            $res = $expe->create($user);
+            
+            foreach ($arrayOrders as $id){
+                $res = $expe->add_object_linked('commande', $id);
+                $comm->fetch($id);
 
-            if($expe->id > 0){
-                foreach ($arrayOrders as $id){
-                    $res = $expe->add_object_linked('commande', $id);
-                    $comm->fetch($id);
+                if ($res > 0){
+                    $lines = $comm->lines;
+                    if (empty($lines) && method_exists($comm, 'fetch_lines')) {
+                        $comm->fetch_lines();
+                        $lines = $cmd->lines;
+                    }
+                    
+                    $fk_parent_line = 0;
+                    
+                    // Looping on each line of the order and add each one on the delivery
+                    foreach ($lines as $line){
+                        $desc = ($line->desc ? $line->desc : '');
+                        // We have multiples orders to create only one delivery, we must put the ref of order on the invoice line
+                        $desc = dol_concatdesc($desc, $langs->trans("Order").' '.$comm->ref.' - '.dol_print_date($comm->date, 'day'));
+                        
+                        $product_type = ($line->product_type ? $line->product_type : 0);
 
-                    if ($res > 0){
-                        $lines = $comm->lines;
-                        if (empty($lines) && method_exists($cmd, 'fetch_lines')) {
-                            $cmd->fetch_lines();
-                            $lines = $cmd->lines;
+                        // Reset fk_parent_line for no child products and special product
+                        if (($line->product_type != 9 && empty($line->fk_parent_line)) || $lines[$i]->product_type == 9) {
+                            $fk_parent_line = 0;
                         }
-
-                        $fk_parent_line = 0;
-
-                        // Looping on each line of the order and add each one on the delivery
-                        foreach ($lines as $line){
-                            $desc = ($line->desc ? $line->desc : '');
-                            // We have multiples orders to create only one delivery, we must put the ref of order on the invoice line
-                            $desc = dol_concatdesc($desc, $langs->trans("Order").' '.$cmd->ref.' - '.dol_print_date($cmd->date, 'day'));
-                            
-                            $product_type = ($line->product_type ? $line->product_type : 0);
-
-                            // Reset fk_parent_line for no child products and special product
-							if (($lines[$i]->product_type != 9 && empty($lines[$i]->fk_parent_line)) || $lines[$i]->product_type == 9) {
-								$fk_parent_line = 0;
-							}
-
-                            // Extrafields
-							if (method_exists($line, 'fetch_optionals')) {
-								$line->fetch_optionals();
-								$array_options = $line->array_options;
-							}
-
-                            $expe->context['createfromclone'] = 'createfromclone';
-
-                            $result = $expe->addline(
-                                getDolGlobalInt('MAIN_DEFAULT_WAREHOUSE'),
-                                $line->id,
-                                $line->qty,
-                                $array_options
-                            );
-                            // print "ligne ajoutée produit ".$line->id. ' avec '.$line->qty.' en qty, est-ce ajoutée ? '.$result.'<br>';
-
-                            if ($result > 0){
-                                print 'ligne ajoutée <br>';
-                                $lineid = $result;
-                            } else {
-                                setEventMessages('Erreur lors de l\'ajout du produit '.$line->id, null, 'error');
-                            }
-                            // Define new fk_parent_line
-                            if ($result > 0 && $line->product_type == 9){
-                                $fk_parent_line = $result;
-                            }
+                        
+                        // Extrafields
+                        if (method_exists($line, 'fetch_optionals')) {
+                            $line->fetch_optionals();
+                            $array_options = $line->array_options;
+                        }
+                        
+                        $expe->context['createfromclone'] = 'createfromclone';
+                        
+                        $result = $expe->addline(
+                            getDolGlobalInt('MAIN_DEFAULT_WAREHOUSE'),
+                            $line->id,
+                            $line->qty,
+                            $array_options
+                        );
+                        // print "ligne ajoutée produit ".$line->id. ' avec '.$line->qty.' en qty, est-ce ajoutée ? '.$result.'<br>';
+                        
+                        if ($result > 0){
+                            $lineid = $result;
+                        } else {
+                            setEventMessages('Erreur lors de l\'ajout du produit '.$line->id, null, 'error');
+                        }
+                        // Define new fk_parent_line
+                        if ($result > 0 && $line->product_type == 9){
+                            $fk_parent_line = $result;
                         }
                     }
                 }
-                $db->commit();
-                $texttoshow = $langs->trans('CD_CREATED_EXPE').' (<a href="'.DOL_URL_ROOT.'/expedition/card.php?id='.$expe->id.'">PROV_'.$expe->id.'</a>)';
-				setEventMessages($texttoshow, null, 'mesgs');
             }
 
+            $res = $expe->create($user);
+            
+            if ($res > 0) {
+                $db->commit();
+                $texttoshow = $langs->trans('CD_CREATED_EXPE').' (<a href="'.DOL_URL_ROOT.'/expedition/card.php?id='.$expe->id.'">PROV_'.$expe->id.'</a>)';
+                setEventMessages($texttoshow, null, 'mesgs');
+
+                // Make a redirect to avoid to bill twice if we make a refresh or back
+                $param = '';
+                if (!empty($mode)) {
+                    $param .= '&mode='.urlencode($mode);
+                }
+                if (!empty($contextpage) && $contextpage != $_SERVER["PHP_SELF"]) {
+                    $param .= '&contextpage='.urlencode($contextpage);
+                }
+                if ($limit > 0 && $limit != $conf->liste_limit) {
+                    $param .= '&limit='.((int) $limit);
+                }
+                if ($optioncss != '') {
+                    $param .= '&optioncss='.urlencode($optioncss);
+                }
+                if ($search_all) {
+                    $param .= '&search_all='.urlencode($search_all);
+                }
+                if ($show_files) {
+                    $param .= '&show_files='.urlencode($show_files);
+                }
+                if ($socid > 0) {
+                    $param .= '&socid='.urlencode($socid);
+                }
+                if ($search_status != '') {
+                    $param .= '&search_status='.urlencode($search_status);
+                }
+                if ($search_orderday) {
+                    $param .= '&search_orderday='.urlencode($search_orderday);
+                }
+                if ($search_ordermonth) {
+                    $param .= '&search_ordermonth='.urlencode($search_ordermonth);
+                }
+                if ($search_orderyear) {
+                    $param .= '&search_orderyear='.urlencode($search_orderyear);
+                }
+                if ($search_deliveryday) {
+                    $param .= '&search_deliveryday='.urlencode($search_deliveryday);
+                }
+                if ($search_deliverymonth) {
+                    $param .= '&search_deliverymonth='.urlencode($search_deliverymonth);
+                }
+                if ($search_deliveryyear) {
+                    $param .= '&search_deliveryyear='.urlencode($search_deliveryyear);
+                }
+                if ($search_ref) {
+                    $param .= '&search_ref='.urlencode($search_ref);
+                }
+                if ($search_company) {
+                    $param .= '&search_company='.urlencode($search_company);
+                }
+                if ($search_ref_customer) {
+                    $param .= '&search_ref_customer='.urlencode($search_ref_customer);
+                }
+                if ($search_user > 0) {
+                    $param .= '&search_user='.urlencode($search_user);
+                }
+                if ($search_sale > 0) {
+                    $param .= '&search_sale='.urlencode($search_sale);
+                }
+                if ($search_total_ht != '') {
+                    $param .= '&search_total_ht='.urlencode($search_total_ht);
+                }
+                if ($search_total_vat != '') {
+                    $param .= '&search_total_vat='.urlencode($search_total_vat);
+                }
+                if ($search_total_ttc != '') {
+                    $param .= '&search_total_ttc='.urlencode($search_total_ttc);
+                }
+                // if ($search_project_ref >= 0) {
+                //     $param .= "&search_project_ref=".urlencode($search_project_ref);
+                // }
+                if ($search_billed != '') {
+                    $param .= '&search_billed='.urlencode($search_billed);
+                }
+
+                header("Location: ".$_SERVER['PHP_SELF'].'?'.$param);
+            }
         } else {
             // $comm->error = 'All the orders should have the same thirdparty';
             setEventMessages($langs->trans("CD_NOT_SAME_THIRDPARTY"), null, 'errors');
